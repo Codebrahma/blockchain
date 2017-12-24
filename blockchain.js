b52114c77a6bb175f9be18d4993ec9d45291c905
Nithin Krishna <nithinkrishh@gmail.com>
2017-12-23 19:07:06 -0800

    added linked-list data structure to store block-chain

--- blockchain.js
+++ blockchain.js
@@ -1,23 +1,26 @@
-const Block = require('./block.js');
-const _     = require("underscore");
+const Block    = require('./block.js');
+var LinkedList = require('dbly-linked-list');
 
 const BlockChain = (function(){
-  var _chain = [ 
-    new Block("Genesis Block") 
-  ];
-  
+
+  var _chain = new LinkedList();
+  _chain.insert(new Block("Genesis Block"));
+
   function BlockChain(){
     return this;
   };
 
   BlockChain.prototype.addBlock = function(data){
-    const prevHash = _chain[ _chain.length - 1 ].getHash();
-    _chain.push(new Block(data, prevHash));
+    const prevHash = _chain.getTailNode()
+                           .getData()
+                           .getHash();
+
+    _chain.insert(new Block(data, prevHash));
   };
 
   BlockChain.prototype.print = function(){
-    _.each(_chain, function(c){ c.print() });
-  }
+    _chain.forEach(function(l){ l.getData().print(); });
+  };
 
   return BlockChain;
 }());
