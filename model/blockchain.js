622b165cdba4c8c65df9ad2ab5bb99ca5eddbc29
Nithin Krishna <nithinkrishh@gmail.com>
2017-12-28 04:41:00 -0600

    blockchain db integration

--- model/blockchain.js
+++ model/blockchain.js
@@ -1,29 +1,51 @@
-const Block    = require('./block.js');
-var LinkedList = require('dbly-linked-list');
+const DB         = require('./db.js');
+const Block      = require('./block.js');
+const LinkedList = require('dbly-linked-list');
 
-(function () {
 
+(function(){
 
   function BlockChain(){
-    this._chain = new LinkedList();
-    let _tempBlock = new Block("Genesis Block");
-    _tempBlock.mine();
-    this._chain.insert(_tempBlock);
+    this._chain = new DB();
   };
 
   BlockChain.prototype = {
+
+    init: function(){
+      var initChain = function(empty){
+        // Do nothing if chain exists ( ie, not empty )
+        if(!empty) return;
+        // Create genesis block
+        let _gBlock = new Block("Genesis Block");
+        // append block to the block chain
+        return this.append(_gBlock);
+      }.bind(this);
+
+      return this._chain.$isEmpty().then(initChain);
+    },
+
     addBlock: function(data){
-      const prevHash = this._chain.getTailNode()
-                            .getData()
-                            .getHash();
-      var temp = new Block(data, prevHash);
-      temp.mine();
-      this._chain.insert(temp);
+      var addBlockWithLink = function(prevBlock){
+        // get hash of previous block
+        let prev  = prevBlock.getHash();
+        // create a new block pointed to prev hash
+        var temp = new Block(data, prev);
+        // append block to the block chain
+        return this.append(temp);
+      }.bind(this);
+
+      return this._chain.$fetchLast().then(addBlockWithLink);
+    },
+
+    append: function(block){
+      // append block to the block chain after consensus
+      return this._chain.$append(block.mine());
     },
 
     print: function(){
-      this._chain.forEach(function(l){ l.getData().print(); });
-    }
+      // iterate through the chain and print each block
+      return this._chain.$forEach(function(l){ l.print(); });
+    },
   };
 
   module.exports = BlockChain;
