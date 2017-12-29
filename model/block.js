1c6d7efe0e90ecf80936efb1d5be620b48fa0182
Nithin Krishna <nithinkrishh@gmail.com>
2017-12-29 03:28:35 -0600

    removed immutable map

--- model/block.js
+++ model/block.js
@@ -1,79 +1,79 @@
-const { Map }  = require('immutable');
-const crypto   = require('crypto');
+const _       = require('underscore');
+const Crypto  = require('./util/crypto.js');
+const Util    = require('./util/bc_util.js');
 
 (function(){
 
   let MAX_NONCE = 9223372036854776000; // MaxInt64 = 2^63 - 1
 
-  function Hashify(obj){
-    const headers = [
-      obj.get('prevBlockHash'),
-      obj.get('data'),
-      obj.get('timeStamp'),
-      obj.get('nonce'),
-      obj.get('difficulty'),
-    ].join();
-
-    return crypto.createHmac('sha256', headers).digest('hex')
-  };
-
   function Block(data, prevBlockHash="", difficulty=4, nonce=0, timeStamp=(new Date()), hash=""){
-    this._block = Map({
-      "prevBlockHash": String(prevBlockHash),
-      "data"         : String(data),
-      "timeStamp"    : String(timeStamp),
-      "nonce"        : nonce,
-      "difficulty"   : difficulty,
-      "hash"         : hash,
-    });
+    this._block = { };
+    this._block["data"]         = Util.cleanData(data);
+    this._block["prevBlockHash"]= prevBlockHash;
+    this._block["difficulty"]   = difficulty;
+    this._block["nonce"]        = nonce;
+    this._block["timeStamp"]    = String(timeStamp);
+    this._block["hash"]         = hash;
   };
  
-
   // Instance methods
   Block.prototype = {
 
     getHash: function(){
-      return this._block.get("hash");
+      return this._block["hash"];
     },
 
     hashify: function(){
-      return Hashify(this._block);
+      const headers = [
+        'prevBlockHash',
+        'data',
+        'timeStamp',
+        'nonce',
+        'difficulty',
+      ];
+
+      let headerStr = _.chain(headers).sort()
+                       .map(function(v){ return this._block[v] })
+                       .value().join();
+
+      return Crypto.hashify(headerStr);
     },
 
     getPrevHash: function(){
-      return this._block.get("prevBlockHash");
+      return this._block["prevBlockHash"];
     },
 
     mine: function(){
       for(let _nonce = 0 ; _nonce < MAX_NONCE && !this.validate(); _nonce++){
-        this._block = this._block.set("nonce", _nonce);
+        this._block["nonce"] = _nonce;
       };
-      this._block = this._block.set("hash", this.hashify());
+      this._block["hash"] = this.hashify();
       return this;
     },
 
     validate: function(){
       let _hash   = this.hashify();
-      let d       = this._block.get("difficulty");;
+      let d       = this._block["difficulty"];
       return _hash.substring(0, d) === '0'.repeat(d);
     },
 
     serialize: function(){
-      return this._block.toObject();
+      return this._block;
     },
 
     print: function(verbose){
 
       if(verbose){
-        console.log("Data          : " + this._block.get("data"));
-        console.log("Time Stamp    : " + this._block.get("timeStamp"));
-        console.log("PrevBlockHash : " + this._block.get("prevBlockHash"));
-        console.log("Nonce         : " + this._block.get("nonce"));
-        console.log("Hash          : " + this._block.get("hash"));
+        console.log("Hash          : " + this._block["hash"]);
+        console.log("Data          : " + this._block["data"]);
+        console.log("PrevBlockHash : " + this._block["prevBlockHash"]);
+        console.log("Nonce         : " + this._block["nonce"]);
+        console.log("Difficulty    : " + this._block["difficulty"]);
+        console.log("Time Stamp    : " + this._block["timeStamp"]);
         console.log("PoW?          : " + this.validate());
         console.log("---------------------------------------------------------------------------------");
       } else {
-        process.stdout.write(this._block.get("data"));
+        process.stdout.write(this._block["data"]);
         this.getPrevHash() ? process.stdout.write(" => ") : process.stdout.write(" ||\n");
       };
 
